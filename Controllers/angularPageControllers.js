7a95cd70a1f16e790a5877575f5a620171340b32
Ignacio <ignaciosabiajes@gmail.com>
2023-10-28 10:36:29 -0300

    register user intitialized

--- Controllers/angularPageControllers.js
+++ Controllers/angularPageControllers.js
@@ -2,3 +2,26 @@ const { defaults } = require("pg");
 const knex = require("../config/knexfile");
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+
+
+//registerUser
+exports.regUser  = async(req,res) =>{
+    const salt = await bcrypt.genSalt(10);
+    const password = await bcrypt.hash(req.body.password, salt);
+    const newUser = {
+        mail: req.body.mail,
+        password:password
+    }
+
+    try {
+        knex('User')
+        .insert({
+            email : newUser.mail,
+            password: newUser.password
+        })
+        .then(res.json({message: "Success!"}))
+    } catch (error) {
+        res.json({error:error.message})
+        
+    }
+}
