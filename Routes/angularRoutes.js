edc26a738fb84f407dd4e951a6ce826f7db3d085
Ignacio <ignaciosabiajes@gmail.com>
2023-10-30 10:45:21 -0300

    register route added

---
+++ Routes/angularRoutes.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const routes = express.Router();
+const {verifyToken} = require('../middlewares/authUser');
+const { regUser } = require("../Controllers/angularPageControllers");
+
+
+
+//registerUser
+routes.post('register', regUser);
